42cb5c7ff7bde9eed938d2b6fb6720e9cbdd25a6
lperalta <lperalta@ad.engr.wisc.edu>
2023-12-16 12:03:32 -0600

    updated mem.v to instantiate cachemem and cacheinterface- still needs work

--- src/PipelineStages/MEM/MEM.v
+++ src/PipelineStages/MEM/MEM.v
@@ -2,7 +2,7 @@ module MEM (clk,
             rst,
             Mem_En,
             Mem_Wr,
-            MemStall,
+            MemStall, //stall signal from somewhere else?
             OpcodeIn,
             WBOpcodeIn,
             SrcReg1,
@@ -12,17 +12,20 @@ module MEM (clk,
             ALUOut,
             PipelineDataOut,
             MemOut,
-            CacheStall,
-            MemoryRequest);
+            CacheStall);
 
   input clk, rst, Mem_En, Mem_Wr, MemStall;
   input [3:0] SrcReg1, OpcodeIn, WBOpcodeIn, WB_RegWrite;  
   input [15:0] ALUOut, RegRead1, WB_MemOut;
   output [15:0] MemOut, PipelineDataOut;
-  output CacheStall, MemoryRequest;
+  output CacheStall;
 
+//Internal wires
   wire MemToMemFwd;
-  wire [15:0] RegRead;
+  wire [15:0] RegRead, i_cache_address, d_cache_address, mem_request_address;
+  wire MemoryRequest, mem_ready, i_cache_request, d_cache_request;
+  wire i_cache_grant, d_cache_grant;
+  wire i_cache_stall, d_cache_stall;
 
   assign MemToMemFwd = (OpcodeIn == 4'b1001) & 
                        (WBOpcodeIn == 4'b1000) & 
@@ -39,12 +42,32 @@ module MEM (clk,
                      .rst(rst));
                      **/
 
+
+  // assign i_cache_request = //how do we assign these?
+  // assign d_cache_request = 
+  // assign i_cache_address = 
+  // assign d_cache_address = 
+
+  CacheMemoryInterface(.clk(clk),
+                        .rst(rst),
+                        .i_cache_request(i_cache_request),
+                        .i_cache_address(i_cache_address),
+                        .d_cache_request(d_cache_request),
+                        .mem_ready(mem_ready),
+                        .i_cache_grant(i_cache_grant),
+                        .i_cache_stall(i_cache_stall),
+                        .d_cache_grant(d_cache_grant),
+                        .d_cache_stall(d_cache_stall),
+                        .mem_address_out(MemOut),
+                        .mem_request(MemoryRequest));
+
+
  CacheInterface(.clk(clk),
                 .rst(rst),
-                .PipelineDataIn(PipelineDataIn),
-                .PipelineAddressIn(PipelineDataOut),
+                .PipelineDataIn(RegRead),               // difference between pipeline data in 
+                .PipelineAddressIn(mem_request_address), //and memory data in?
                 .MemoryDataIn(RegRead),
-                .MemoryAddressIn(ALUOut),
+                .MemoryAddressIn(MemOut),
                 .CacheWriteEnable(Mem_Wr),
                 .MemStall(MemStall),
                 .PipelineDataOut(PipelineDataOut),
